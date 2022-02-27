c99f6454e611146e96314011987dcdae592410d3
DawsonVaal <dcvaal@bsu.edu>
2022-02-26 21:28:20 -0500

    Setup gradle and created an application class. Import javaFX tools.

--- src/main/java/TimpkntTampkntTompkt.java
+++ src/main/java/TimpkntTampkntTompkt.java
@@ -1,2 +1,9 @@
-public class TimpkntTampkntTompkt {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TimpkntTampkntTompkt extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Hey");
+    }
 }
